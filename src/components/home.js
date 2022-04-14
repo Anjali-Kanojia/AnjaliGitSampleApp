51685b37179b355154251520a966a0d764e16cbe
Anjali Kanojia <anjalikanojia09@gmail.com>
2022-04-13 17:36:40 -0700

    console

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text, Button} from 'react-native'
 
 function Home() {
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
         <Text>Hello Git</Text>
+        <Button title="Click for Goodbye"/>
     </View>
   )
 }
